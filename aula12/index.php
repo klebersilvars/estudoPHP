6b178628f55b12e82327fedd983f04919bd35808
klebersilvars <kleber.600.l@gmail.com>
2023-10-20 23:19:57 -0300

    aulaVista
    wda

---
+++ aula12/index.php
@@ -0,0 +1,30 @@
+<?php
+
+//operadores de atribuição
+
+
+/*
+$nome = 'kleber';
+*/
+//operadores aritméticos
+//serve para realizar operações matemáticas
+/*
+$idade = 29;
+$ano = 2023;
+*/
+
+/*///fazendo uma soma entre as idades e o ano
+$resultado = $idade + $ano;
+echo $resultado;*/
+
+/*//fazendo subtração em php
+$zona = 28;
+$tabela = 54;
+$resultadoBrasileirao = $tabela - $zona;
+echo $resultadoBrasileirao; */
+
+//operadores de comparação
+
+// == (significa que o militar que está sendo avaliado é igual a outra coisa)
+
+//de lima preocupado para saber do filho dela que está punido
