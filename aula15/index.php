0e0fb70cf39ce129136159e0a69c01715897ff62
klebersilvars <kleber.600.l@gmail.com>
2023-10-22 00:20:08 -0300

    dd
    dd

---
+++ aula15/index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+//loop  = estrutura de repetição
+
+$num1 = 3;
+$num2 = 4;
+
+$resultado = 0;
+
+for($i = 0; $i < $num2; $i++) { //código vai rodar até o i chegar ao num2
+    $resultado = $resultado + $num2; 
+}
+
