8e86ddb2bd22a10c0a3b25300a39ce9c83352a10
klebersilvars <kleber.600.l@gmail.com>
2023-10-22 19:24:21 -0300

    22/10/23
    estudo da aula 17 finalizado

---
+++ aula17/index.php
@@ -0,0 +1,24 @@
+
+<?php
+
+//como consigo montar um array?
+
+$lista2 = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+
+//como consigo mostrar esses arrays na tela?
+
+//var_dump($lista); 
+//var_dump($lista2);
+
+//como excluir um item de um array
+
+/*$nova = array_pop($lista2);
+echo ($nova);
+var_dump($lista2);*/
+
+//como consigo colocar algo dentro do meu array?
+
+array_push($lista2, 'novo valor');
+var_dump($lista2);
+
