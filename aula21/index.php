08da4b16a2962baa26855a4f281d198f1ecfbe85
klebersilvars <kleber.600.l@gmail.com>
2023-10-25 21:14:13 -0300

    ff

---
+++ aula21/index.php
@@ -0,0 +1,10 @@
+
+<p>O nome é: <?php echo $_GET['nome']; ?></p>
+<p>A idade é <?php echo $_GET['idade']; ?></p>
+<p>O seu e-mail é <?php echo $_GET('conseguiram só agora');
+
+//O QUE É O MÉTODO GET?
+
+//serve para eu recuperar alguns dados
+
+echo $_GET['nome'];
